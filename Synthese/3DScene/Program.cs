aa0dee383883e56a015075ffaa6a629f36136575
Antoine Henry <anteoinenry@hotmail.com>
2018-10-01 13:49:17 +0200

    Scene modification

--- Synthese/3DScene/Program.cs
+++ Synthese/3DScene/Program.cs
@@ -13,11 +13,12 @@ namespace TPSynthese
             List<Sphere> spheres = new List<Sphere>();
             spheres.Add(new Sphere(.5f, new Vector3(5f, 15f, -10f), Color.Yellow, 500f));
             spheres.Add(new Sphere(.5f, new Vector3(-5f, 15f, -10f), Color.OrangeRed, 500f));
-            spheres.Add(new Sphere(.5f, new Vector3(0f, 20f, -10f), Color.White, 500f));
+            spheres.Add(new Sphere(.5f, new Vector3(0f, 20f, -10f), Color.DeepSkyBlue, 500f));
             spheres.Add(new Sphere(1f, new Vector3(0f, 5f, -10f), Color.ForestGreen, 0f));
             spheres.Add(new Sphere(5f, new Vector3(3f, -5f, -8f), Color.Tomato, 0f));
             spheres.Add(new Sphere(10f, new Vector3(-15f, 0f, 0f), Color.Purple, 0f));
             spheres.Add(new Sphere(20f, new Vector3(0f, -25f, 0f), Color.Beige, 0f));
+            spheres.Add(new Sphere(50f, new Vector3(50f, 0f, 20f), Color.DodgerBlue, 0f));
 
             Camera camera = new Camera();
             camera.position = new Vector3(0f, 0f, -50f);
