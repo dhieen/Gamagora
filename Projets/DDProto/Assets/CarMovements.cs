fa8f97b6e94cfcf7c5c89a16d73b010e84c07705
DESKTOP-KUO1DJ7\anteo <antoinenry@gmail.com>
2018-12-09 17:18:32 +0100

    Devil drift proto - First commit

---
+++ Projets/DDProto/Assets/CarMovements.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovements : MonoBehaviour {
+    
+    public float motorTorque;
+    public float steerAngle;
+    public float driftTorque;
+    public Vector3 dashVelocity;
+
+    public Rigidbody carBody;
+    public List<WheelCollider> motorWheels;
+    public List<WheelCollider> steerWheels;
+
+    private bool dashing;
+	
+	
+	void FixedUpdate ()
+    {
+        float accelerate = Input.GetAxisRaw("Vertical");
+        float steer = Input.GetAxisRaw("Horizontal");
+        bool drift = Input.GetButton("Fire1");
+        bool dash = Input.GetButtonDown("Fire2");
+
+        foreach (WheelCollider wc in motorWheels)
+        {
+            wc.motorTorque = motorTorque * accelerate;
+        }
+
+        foreach (WheelCollider wc in steerWheels)
+            wc.steerAngle = steer * steerAngle;
+
+        if (drift)
+            carBody.AddRelativeTorque(Vector3.up * driftTorque * steer);
+
+        if (dash)
+        {
+            if (steer == 0f)
+                carBody.velocity = carBody.velocity + carBody.rotation * new Vector3(0, dashVelocity.y, dashVelocity.z);
+            else
+                carBody.velocity = carBody.velocity + carBody.rotation * new Vector3(-steer * dashVelocity.x, dashVelocity.y, 0f);
+        }
+	}
+}
---
+++ Projets/DDProto/Assets/StalkCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StalkCamera : MonoBehaviour
+{
+    public Transform target;
+    public float distance;
+    public float height;
+    public float moveForce;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.rotation = Quaternion.LookRotation(target.position - transform.position, Vector3.up);
+        rb.velocity = moveForce * (target.position - target.rotation * Vector3.forward * distance - transform.position).normalized;
+    }
+
+}
